25ddc05e6b865639bc1fdc9e808c44bc9c5091c8
Hjt <ronaldohjt2@gmail.com>
2024-03-11 17:52:10 +0330

    counter class added
    fixed
    tested
    working fine

--- src/main.py
+++ src/main.py
@@ -1,7 +1,13 @@
-import module
+from module import *
 
 def main():
     print('Hello, World!')
+    counter = Counter(1, 1, -1)
+    counter()
+    counter()
+    counter()
+    counter(False)
+    print(counter.count)
 
 if __name__ == '__main__':
     main()
