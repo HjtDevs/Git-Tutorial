e41cd8e1d996bb2c1c93fca26f174ad9d301deb5
Hjt <ronaldohjt2@gmail.com>
2024-03-12 11:09:52 +0330

    Added logging to Counter class

--- src/module.py
+++ src/module.py
@@ -1,14 +1,16 @@
-from typing import Any
-
+from datetime import datetime
 
 class Counter:
     def __init__(self, count: int | float = 0.0, increasement: int | float = 1.0, decreasement: int | float = -1.0) -> None:
         self.count = count
         self.increaser = increasement
         self.decreaser = decreasement
+        self.log = []
 
     def __call__(self, increase: bool = True):
         if increase:
             self.count += self.increaser
+            self.log.append(f'Incresed Value from {self.count - 1} to {self.count} at {datetime.now()}')
             return
         self.count += self.decreaser
+        self.log.append(f'Decresed Value from {self.count + 1} to {self.count} at {datetime.now()}')
